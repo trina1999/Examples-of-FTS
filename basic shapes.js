70cc27cc3472ce42cc7d3790d4330a2c53acace3
Trina Mahapatra <99464198+trina1999@users.noreply.github.com>
2022-10-14 12:08:44 +0530

    Create basic shapes.js

---
+++ basic shapes.js
@@ -0,0 +1,9 @@
+function setup() {
+createCanvas(400, 150);
+}
+function draw() {
+background(204);
+quad(158, 55, 199, 14, 392, 66, 351, 107);
+triangle(347, 54, 392, 9, 392, 66);
+triangle(158, 55, 290, 91, 290, 112);
+}
